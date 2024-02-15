6bde660ccc787ae69db907b462e4687af344336b
aleksandr.smirnov <ggrelaxi@gmail.com>
2024-02-15 20:18:43 +0300

    BUG. fix prime algo

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,8 +5,9 @@ from ..engine import run_engine
 def is_prime(num):
     if num < 2:
         return False
-    for i in range(num // 2):
-        if num % 2 == 0:
+    for i in range(2, (num // 2) + 1):
+        print(i)
+        if num % i == 0:
             return False
     return True
 
