55e33a0118435f2e5da7bb8fbb07ff3991142a97
aleksandr.smirnov <ggrelaxi@gmail.com>
2024-02-15 20:07:36 +0300

    finish stage8

--- Makefile
+++ Makefile
@@ -7,6 +7,18 @@ brain-games:
 brain-even:
 	poetry run brain-even
 
+brain-prime:
+	poetry run brain-prime
+
+brain-calc:
+	poetry run brain-calc
+
+brain-progression:
+	poetry run brain-progression
+
+brain-gcd:
+	poetry run brain-gcd
+
 build:
 	poetry build
 
---
+++ brain_games/games/prime.py
@@ -0,0 +1,23 @@
+from random import randint
+from ..engine import run_engine
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(num // 2):
+        if num % 2 == 0:
+            return False
+    return True
+
+def generate_round():
+    number = randint(1, 100)
+    answer = 'yes' if is_prime(number) else 'no'
+    question = str(number)
+
+    return (question, answer)
+
+
+def main():
+    rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    run_engine(rules, generate_round)
